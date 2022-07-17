e31a1a5a5e5b4b3db399680c6d09e16fc9ae4883
Guillermo Nicrosi <erknardsieghartrage@gmail.com>
2022-07-16 22:58:53 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const axios = require ("axios");
 
 // Syncing all the models at once.
 
-conn.sync({ force: true }).then(async() => {
+conn.sync({ force: false }).then(async() => {
   server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
